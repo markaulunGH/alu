347a695fa9d919ac1062273fff0b96f0d9e5ac33
markaulunGH <markaulun@qq.com>
2022-07-05 18:16:39 +0800

    pass test walloc_booth

--- mul_model/Wallace_booth/wallace_booth.v
+++ mul_model/Wallace_booth/wallace_booth.v
@@ -13,7 +13,7 @@ endmodule
 
 
 
-module partial_34bits(
+/*module partial_34bits(
   input [67:0] x,
   input [33:0] y,
 
@@ -46,7 +46,7 @@ booth_partial part16 (.x_src (x), .y_src (y[33:31]), .p_result (partial_out16), 
 
 
 endmodule
-
+*/
 module switch_17x68bits(
    input [1155:0] part_group,
 
@@ -68,8 +68,8 @@ assign {po16,po15,po14,po13,po12,po11,po10,po09,po08,po07,po06,po05,po04,po03,po
 
 genvar x;
 generate for ( x =0;x<68;x=x+1) begin : gen_switch
-    assign sw_group[(x+1)*17-1:x] = {po16[x],po15[x],po14[x],po13[x],po12[x],po11[x],po10[x],po09[x],
-                                     po08[x],po07[x],po05[x],po04[x],po03[x],po02[x],po01[x],po00[x]};
+    assign sw_group[(x+1)*17-1:x*17] = {po16[x],po15[x],po14[x],po13[x],po12[x],po11[x],po10[x],po09[x],
+                                     po08[x],po07[x],po06[x],po05[x],po04[x],po03[x],po02[x],po01[x],po00[x]};
 end endgenerate
 
 
@@ -95,7 +95,10 @@ wire ready_to_doing;
 assign ready_to_doing = in_ready && in_valid;
 always@(posedge clk ) begin
       if (reset ) begin
-        in_ready <=1;
+        in_ready <=1'b0;
+      end
+      else begin
+        in_ready<=1'b1;
       end
 
       if (ready_to_doing) begin
@@ -111,8 +114,8 @@ end
 
 always @(posedge clk ) begin
   if (ready_to_doing) begin
-    multiplicand <= {{WIDTH{src2[COMPUTER_WIDTH]}},src1};
-    multiplier <= {src1[COMPUTER_WIDTH],src1,1'b0};
+    multiplicand <= {{WIDTH{src1[COMPUTER_WIDTH]}},src1};
+    multiplier <= {src2[COMPUTER_WIDTH],src2};
   end
 end
 
@@ -123,12 +126,15 @@ end
 wire [1155:0] part_group;
 wire [16:0]   part_cout;
 
-booth_partial part0 (.x_src (multiplicand), .y_src({multiplier[1:0]}), 
+booth_partial part0 (.x_src (multiplicand), .y_src({multiplier[1:0],1'b0}), 
                         .p_result (part_group[67:0]), .cout(part_cout[0]));
 genvar x;
-generate for ( x =1;x<17;x=x+1) begin : gen_partial
-booth_partial part (.x_src (multiplicand), .y_src({multiplier[(x+1)*2:x*2]}), 
-                    .p_result (part_group[(x+1)*68:x*68]), .cout(part_cout[x]));
+generate for ( x =1;x<17;x=x+1) begin : gen_walloc_partial
+booth_partial part (
+                    .x_src ({multiplicand[2*WIDTH-1-2*x:0],
+                    {2*x{1'b0}}}), 
+                    .y_src({multiplier[(x+1)*2-1:x*2-1]}), 
+                    .p_result (part_group[(x+1)*68-1:x*68]), .cout(part_cout[x]));
     
 end endgenerate
 
@@ -137,19 +143,19 @@ wire [1155:0] swo_group;
 switch_17x68bits switch (.part_group (part_group), .sw_group (swo_group));
 
 //////////walloc_tree////////////
-wire [937:0] wt_cout;
+wire [937:0] wt_cout;//14bitsx68
 wire [66:0]  wt_c;
 wire [67:0]  wt_s;
 
 walloc_17bits walloc00 (.src_in (swo_group[16:0]), .cin({part_cout[13:0]}), 
-                    .cout_group (),  .cout(), .s (wt_s[67]));
+                    .cout_group (wt_cout[13:0]),  .cout(wt_c[0]), .s (wt_s[0]));
 
 genvar y;
 generate for ( y =1;y<67;y=y+1) begin : gen_walloc
-walloc_17bits walloc  (.src_in (swo_group[(x+1)*17-1:x*17]), .cin({wt_cout[x*14-1:(x-1)*14]}), 
-                    .cout_group (wt_cout[(x+1)*14-1:x*14]),  .cout(wt_c[x]), .s (wt_s[x]));
+walloc_17bits walloc  (.src_in (swo_group[(y+1)*17-1:y*17]), .cin({wt_cout[y*14-1:(y-1)*14]}), 
+                    .cout_group (wt_cout[(y+1)*14-1:y*14]),  .cout(wt_c[y]), .s (wt_s[y]));
 end endgenerate
-walloc_17bits walloc67(.src_in (swo_group[1155:1087]), .cin({wt_cout[937:924]}), 
+walloc_17bits walloc67(.src_in (swo_group[1155:1139]), .cin({wt_cout[937:924]}), 
                     .cout_group (),  .cout(), .s (wt_s[67]));
 
 //////////////////////////////////second_buf/////////////////////////////////////
